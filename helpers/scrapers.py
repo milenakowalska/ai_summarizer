b46829f9e6afbccca6f91aa22a6b54d392767c80
Milena Kowalska <milenakowalska126@gmail.com>
2026-05-12 18:35:44 +0200

    Create tutorial from article

--- helpers/scrapers.py
+++ helpers/scrapers.py
@@ -5,11 +5,19 @@ import requests
 def fetch_website_content_and_links(url: str):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
+    title_and_content = get_website_content_from_soup(soup)
     return {
-        "content": get_website_content_from_soup(soup),
+        "title": title_and_content["title"],
+        "content": title_and_content["content"],
+        "url": url,
         "links": get_links_from_soup(soup)
     }
 
+def fetch_website_content(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    title_and_content = get_website_content_from_soup(soup)
+    return title_and_content["title"] + "\n" + title_and_content["content"]
 
 def get_website_content_from_soup(soup: BeautifulSoup):
     title = soup.title.string if soup.title else "No title found"
@@ -19,7 +27,10 @@ def get_website_content_from_soup(soup: BeautifulSoup):
         text = soup.body.get_text(separator="\n", strip=True)
     else:
         text = ""
-    return (title + "\n\n" + text)[:2000]
+    return {
+        "title": title,
+        "content": text[:2000]
+    }
 
 
 def get_links_from_soup(soup: BeautifulSoup):
